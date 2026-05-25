e38ca25426b47204d10ca98dd58a135f4562aad9
Alex Toa <christophertoa2019@gmail.com>
2026-05-25 10:40:52 -0500

    feat: Initial commit of project files

--- backend/NotificationService/NotificationService/Data/AppDbContextFactory.cs
+++ backend/NotificationService/NotificationService/Data/AppDbContextFactory.cs
@@ -8,7 +8,7 @@ public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
     public AppDbContext CreateDbContext(string[] args)
     {
         var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
-        optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=UtaAlerta_Notifications;Trusted_Connection=True;TrustServerCertificate=True");
+        optionsBuilder.UseSqlServer("Server=localhost;Database=UtaAlerta_Notifications;Trusted_Connection=True;TrustServerCertificate=True");
 
 
         return new AppDbContext(optionsBuilder.Options);
